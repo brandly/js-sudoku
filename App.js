79aedd832f1dca50f1fe2c20c2cc39afdbb5e76f
brandly <brandly13@gmail.com>
2020-02-26 16:27:02 -0500

    make game look decent

--- App.js
+++ App.js
@@ -8,40 +8,10 @@ const App = () => {
     '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
   )
   return (
-    <div>
-      <table>
-        <tbody>
-          {board.map((row, y) => (
-            <tr key={y}>
-              {row.map((cell, x) => (
-                <td key={x}>
-                  {Array.from(cell).map(num => (
-                    <span
-                      onClick={() => {
-                        setBoard(propagate(new Set([num]), { x, y }, board))
-                      }}
-                    >
-                      {num}
-                    </span>
-                  ))}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button
-        onClick={() => {
-          let solution = search(board)
-          if (solution) {
-            setBoard(solution)
-          }
-        }}
-      >
-        search
-      </button>
-
+    <div className="container">
+      <h1>Sudoku</h1>
       <form
+        className="row"
         onSubmit={e => {
           e.preventDefault()
           setBoard(fromString(puzzleInput))
@@ -54,7 +24,48 @@ const App = () => {
             setPuzzle(e.target.value)
           }}
         />
+        <input type="submit" value="View Puzzle" />
       </form>
+      <table>
+        <tbody>
+          {board.map((row, y) => (
+            <tr key={y}>
+              {row.map((cell, x) => (
+                <td key={x} className={`c${x + 1} r${y + 1}`}>
+                  {cell.size > 1 ? (
+                    <div className="remaining">
+                      {Array.from(cell).map(num => (
+                        <span
+                          onClick={() => {
+                            setBoard(propagate(new Set([num]), { x, y }, board))
+                          }}
+                        >
+                          {num}
+                        </span>
+                      ))}
+                    </div>
+                  ) : null}
+                  <p className="answer">
+                    {cell.size === 1 ? Array.from(cell)[0] : '\u00a0'}
+                  </p>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="row">
+        <button
+          onClick={() => {
+            let solution = search(board)
+            if (solution) {
+              setBoard(solution)
+            }
+          }}
+        >
+          Find Solution
+        </button>
+      </div>
     </div>
   )
 }
